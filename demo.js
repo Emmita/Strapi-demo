346db71d59948dc808eae5155220fe190d36d912
Emmita <emmanuelavilah@gmail.com>
2019-03-06 17:04:14 -0600

    file changed

--- demo.js
+++ demo.js
@@ -1,11 +1,34 @@
+var os = process.platform;
 var exec = require("child_process").exec;
-exec(
-  'C:/"Program Files"/MongoDB/Server/4.0/bin/mongodump --db demo_database_two --out C:/Users/e-mjavila/Desktop',
-  function(error, stdout, stderr) {
-    console.log("stdout: " + stdout);
-    console.log("stderr: " + stderr);
-    if (error !== null) {
-      console.log("exec error: " + error);
+
+if(os === 'linux'){
+  exec(
+    'sudo mongodump --db demo_database --out /home/emma/Documents',
+    function(error, stdout, stderr) {
+      console.log(os);
+      console.log("stdout: " + stdout);
+      console.log("stderr: " + stderr);
+      if (error !== null) {
+        console.log("exec error: " + error);
+      }
     }
-  }
-);
+  );
+
+}else if(os === 'win32'){
+  exec(
+    'C:/"Program Files"/MongoDB/Server/4.0/bin/mongodump --db demo_database --out C:/Users/e-mjavila/Documents',
+    function(error, stdout, stderr) {
+      console.log(os);
+      console.log("stdout: " + stdout);
+      console.log("stderr: " + stderr);
+      if (error !== null) {
+        console.log("exec error: " + error);
+      }
+    }
+  );
+
+}
+
+
+
+
